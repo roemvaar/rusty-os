0d646b8a45a6db24d6741fb570202cfa432a13e2
Roberto Valenzuela <valenzuelarober@gmail.com>
2023-04-29 19:31:06 -0400

    VGA module printing to QEMU prompt

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,23 @@
 #![no_std]
 #![no_main]
 
+
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World! This is Rusty-OS :)";
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // println!("Rusty OS!");
-    // println!("Rusty-OS is a simple RTOS for ARM-based embedded systems written in Rust.");
+    println!("Rusty OS!");
+    println!("Rusty-OS is a simple RTOS for ARM-based embedded systems written in Rust.");
     
-    // let mut ticks = 0;
+    let mut ticks = 0;
     
-    // // Ifinite loop
-    // loop {
-    //     // Write your tasks here   
-    //     println!("Awake time: {} ticks", ticks);
-    //     ticks = ticks + 1;
-    // }
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
+    // Ifinite loop
+    loop {
+        // Write your tasks here   
+        println!("Awake time: {} ticks", ticks);
+        ticks = ticks + 1;
     }
 
     loop {}
