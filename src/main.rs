38ee0a0e7315bc38cc2969a541a2bf302161f96f
Roberto Valenzuela <valenzuelarober@gmail.com>
2023-04-29 20:23:05 -0400

    Add serial module for unit testing

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,92 @@
 
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
+mod serial;
 mod vga_buffer;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Rusty OS!");
     println!("Rusty-OS is a simple RTOS for ARM-based embedded systems written in Rust.");
 
+    #[cfg(test)]
+    test_main();
+
     let mut ticks = 0;
 
-    // Ifinite loop
     loop {
         // Write your tasks here
         println!("Awake time: {} ticks", ticks);
         ticks = ticks + 1;
     }
-
-    loop {}
 }
 
 // This function is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+// Panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(0, 1);
+    serial_println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
 }
