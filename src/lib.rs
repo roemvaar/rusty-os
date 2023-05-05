7fcb037c9535241f630924b1e4a718b9d6dff1a4
Roberto Valenzuela <valenzuelarober@gmail.com>
2023-05-05 16:59:56 -0400

    CPU exception handling with tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 
@@ -61,6 +63,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
@@ -69,4 +72,8 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info);
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
